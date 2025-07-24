28c8655ba6790bfe697d679cdabbb7a28664ba0d
dolam77 <dolsam77@nate.com>
2025-07-24 12:25:14 +0900

    second commit

---
+++ src/main/java/com/example/gitaction_spring/GitactionSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitaction_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitactionSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitactionSpringApplication.class, args);
+	}
+
+}
